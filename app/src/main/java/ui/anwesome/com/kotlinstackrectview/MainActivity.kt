70c543cbacc38f0248550ebb919afa5900d5f07f
Anwesh43 <anweshthecool0@gmail.com>
2018-01-04 23:30:49 +0530

    using movementlistener for view in activity

--- app/src/main/java/ui/anwesome/com/kotlinstackrectview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinstackrectview/MainActivity.kt
@@ -2,12 +2,18 @@ package ui.anwesome.com.kotlinstackrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.stackrectview.StackRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StackRectView.create(this)
+        val view = StackRectView.create(this)
+        view.addRectMovementListener({i ->
+            Toast.makeText(this,"$i moved to left",Toast.LENGTH_SHORT).show()
+        },{i->
+            Toast.makeText(this,"$i moved to right",Toast.LENGTH_SHORT).show()
+        })
     }
 }
